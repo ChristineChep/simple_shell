5a59466dcb27b81d0cfcac26e432443b5c511131
Jackson-Muendo <jackmwexh1225@gmail.com>
2022-05-19 03:21:01 -0700

    shell

--- header.h
+++ header.h
@@ -28,14 +28,5 @@ void _printenv(char **env);
 char **path_finder(char **token_path, char **env_path, char *stringet);
 char *_strcat(char *dest, char *src);
 int childhood(char **token, char *av[], char **env, char *string);
-int (*get_builtin(char *command))(char **args, char **front);
-int shellby_exit(char **args, char **front);
-int shellby_env(char **args, char __attribute__((__unused__)) **front);
-int shellby_setenv(char **args, char __attribute__((__unused__)) **front);
-int shellby_unsetenv(char **args, char __attribute__((__unused__)) **front);
-int shellby_cd(char **args, char __attribute__((__unused__)) **front);
-int shellby_alias(char **args, char __attribute__((__unused__)) **front);
-int shellby_help(char **args, char __attribute__((__unused__)) **front);
-
 
 #endif /* _HEADER_H_ */
