76b0960c334dcad5143b1744136e1b3f07e6d8d4
Jackson-Muendo <jackmwexh1225@gmail.com>
2022-05-18 23:36:07 -0700

    shell program

---
+++ handlesignint.c
@@ -0,0 +1,14 @@
+#include "header.h"
+
+/**
+ * handle_sigint - Signal Handler - Interrupt the process / ctrl + C.
+ * @sig: Received signal.
+ *
+ * Return: Nothing.
+ */
+
+void handle_sigint(int __attribute__((unused)) sig)
+{
+	write(STDOUT_FILENO, "\n$ ", 3);
+	fflush(stdout);
+}
